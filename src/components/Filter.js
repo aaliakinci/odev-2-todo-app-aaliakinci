38aa5191a6cd649e827eb3d679d84cb6de3895b7
aaliakinci <aliakiinci@gmail.com>
2021-01-11 15:42:47 +0300

    improvoment Filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,40 +1,40 @@
-import React, {useEffect } from 'react';
+import React, {useEffect} from 'react';
 
 export default function Filter({ todo,setTodo, setFake, filterStatus, setFilterStatus }) {
- 
-	const filterTodo = () => {
-		switch (filterStatus) {
-			case 'all':
-				setFake(todo);
-				break;
-			case 'active':
-				setFake(todo.filter((x) => x.completed === false));
-				break;
-			case 'completed':
-				setFake(todo.filter((x) => x.completed === true));
-				break;
-			default:
-				break;
-		}
-	};
+	
 	const clearComplete =  () => 
 	{
 		 const newState = todo.filter(x=>x.completed!==true);
 		 setTodo(newState);
 	}
 	useEffect(() => {
-		filterTodo();
-	}, [todo, filterStatus]);
+		const filterTodo = () => {
+			switch (filterStatus) {
+				case 'all':
+					setFake(todo);
+					break;
+				case 'active':
+					setFake(todo.filter((x) => x.completed === false));
+					break;
+				case 'completed':
+					setFake(todo.filter((x) => x.completed === true));
+					break;
+				default:
+					break;
+			}
+		};
+	 	filterTodo();
+	}, [todo, filterStatus,setFake]);
  
 	return (
 		<ul className="filters">
-			<li onClick={() => setFilterStatus('all')}>
+			<li onClick={() => setFilterStatus('all')} style={{marginRight:'5px'}}>
 				 All 
 			</li>
-			<li onClick={() => setFilterStatus('active')}>
+			<li onClick={() => setFilterStatus('active')} style={{marginRight:'5px'}}>
 				 Active 
 			</li>
-			<li onClick={() => setFilterStatus('completed')}>
+			<li onClick={() => setFilterStatus('completed')} style={{marginRight:'5px'}}>
 				 Completed 
 			</li>
 			<li>
